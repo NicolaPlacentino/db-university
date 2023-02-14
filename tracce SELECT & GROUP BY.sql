c9cd7bc75be556d09ffa48cc81843ada29797e53
NicolaPlacentino <nyko0921@gmail.com>
2023-02-14 16:27:42 +0100

    completed exercise 2 - query con group by

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -61,6 +61,9 @@ GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
+SELECT COUNT(*) AS 'insegnanti', `office_address` AS 'edificio ufficio'
+FROM `teachers`
+GROUP BY `office_address`;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 
