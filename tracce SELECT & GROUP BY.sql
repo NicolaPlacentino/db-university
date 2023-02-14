f968d80e7c7346b5083b88e09f0068c9a6ff2aab
NicolaPlacentino <nyko0921@gmail.com>
2023-02-14 16:38:52 +0100

    completed exercise 4 - query con group by

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -72,3 +72,7 @@ FROM `exam_student`
 GROUP BY `exam_id`;
 
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT COUNT(*) AS 'corsi di laurea', `department_id` AS 'dipartimento'
+FROM `degrees`
+GROUP BY `department_id`;
