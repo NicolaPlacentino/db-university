52d14fb65c957a98a981bda842070462c1c368c2
NicolaPlacentino <nyko0921@gmail.com>
2023-02-14 16:23:38 +0100

    completed exercise 1 - query con group by

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -55,6 +55,9 @@ WHERE `phone` IS NULL;
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 
+SELECT COUNT(*) AS 'iscritti', YEAR(`enrolment_date`) AS "anno d'iscrizione"
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
