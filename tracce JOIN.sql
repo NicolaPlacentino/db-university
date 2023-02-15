f721ef17f80baf56ba64e22bdc6eda9afc077692
NicolaPlacentino <nyko0921@gmail.com>
2023-02-15 15:43:04 +0100

    completed exercise JOIN 2

--- tracce JOIN.sql
+++ tracce JOIN.sql
@@ -1,4 +1,4 @@
---1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
+-- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 
 SELECT `students`.`name`, `students`.`surname`, `degrees`.`name`
 FROM `students`
@@ -6,20 +6,25 @@ JOIN `degrees`
 ON `students`.`degree_id` = `degrees`.`id`
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
---2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+-- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+SELECT `degrees`.`name`, `departments`.`name`
+FROM `degrees`
+JOIN `departments`
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
+-- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 
 
---3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
+-- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
 
---4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
 
---5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
 
 
---6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
-
-
---7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
 
